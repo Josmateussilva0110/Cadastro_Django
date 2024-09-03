f8aeb1529f2a7d3d7ec1ac4b49633b1dc9f87a11
Jose Mateus <jmateussilva044@gmail.com>
2024-09-03 09:43:42 -0300

    algumas integrações

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,16 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Person(models.Model):
-    class Meta:
-        verbose_name = 'Person'
-        verbose_name_plural = 'Persons'
-    
-    username = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150, blank=True)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=100)
-
-
-    def __str__(self):
-        return self.first_name
+class Person(AbstractUser):
+    pass
---
+++ projeto_02/cadastro/context_processors.py
@@ -0,0 +1,3 @@
+def page_title(request):
+    default_title = 'Gerenciador'
+    return {'page_title': default_title}
